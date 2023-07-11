22fd9b47674a2e76e5540d4b62e4f3cf0b76bcc0
maya florez <florezmaya03@gmail.com>
2023-07-11 08:27:41 -0500

    Improving my project using pandas

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
+    '''
     data = read_csv.read_csv('./data.csv')
     data = list(filter(lambda item: item['Continent'] == 'South America',data))
 
     countries = list(map(lambda x: x['Country/Territory'],data))
     percentages = list(map(lambda x: x['World Population Percentage'], data))
     charts.generate_pie_chart(countries,percentages)
+    '''
+    df = pd.read_csv('./data.csv')
+    df = df[df['Continent'] == 'Africa']
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries,percentages)
 
+    data = read_csv.read_csv('./data.csv')
     country = input('Type country => ')
     print(country)
 
